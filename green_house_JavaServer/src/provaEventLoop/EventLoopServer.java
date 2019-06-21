9f2b062a5008c2f30a87b54bbe330f38e2a11170
Filippo Orazi <filippo.orazi@studio.unibo.it>
2019-06-21 15:29:51 +0200

    caioooooo

---
+++ green_house_JavaServer/src/provaEventLoop/EventLoopServer.java
@@ -0,0 +1,5 @@
+package provaEventLoop;
+
+public class EventLoopServer {
+
+}
---
+++ green_house_JavaServer/src/provaEventLoop/Main.java
@@ -0,0 +1,11 @@
+package provaEventLoop;
+
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application.launch(Gui.class, args);
+	}
+
+}
